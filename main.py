7f0343233a50e9d3e5853731f844c3b5cd8f991b
Hoori <h.dahesh.2000@gmail.com>
2022-08-28 21:30:49 +0430

    Add multiplication function

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-
-def add(numbers):
+def add1(numbers):
     ADD=0
     for x in numbers:
         x=int(x)
         ADD=x+ADD
     return (ADD)
+"""
+def subtract1(numbers):
+    sub=0
+    for x in numbers:
+        x=int(x)
+        sub=sub-x
+    return (sub)
+"""
+def multiplication1(numbers):
+    mul=1
+    for x in numbers:
+        x=int(x)
+        mul=x*mul
+    return (mul)
+
+#def divide1(numbers):
 
 
 print("Choose one of the options:")
 print("0. Exit")
 print("1. Add")
 print("2. Subtract")
-print("3. Multiply")
+print("3. Multiplication")
 print("4. Divide")
 print()
 
@@ -19,10 +34,17 @@ while (1):
     choice=input("Enter choice:")
     if choice=="0":
         quit()
-    if choice in ("1" or "2" or "3" or "4"):
+    if choice in ("1" , "2" , "3" , "4"):
         print("Enter your numbers:")
         numbers=input().split()
         if choice=="1":
-            print(add(numbers))
+            print("The sum of the numbers is equal to {}".format(add1(numbers)))
+        #elif choice=="2":
+         #   print("The subtract of the numbers is equal to {}".format(subtract1(numbers)))
+        elif choice == "3":
+            print("The multiply of the numbers is equal to {}".format(multiplication1(numbers)))
+        #elif choice=="4":
+         #   print("The subtract of the numbers is equal to {}".format(divide1(numbers)))
+
     else:
         print("This option is invalid")
